4551e2589270a3d680a90624daa9aa4dff0ffa11
SSlime-s <62363188+SSlime-s@users.noreply.github.com>
2024-06-27 17:25:10 +0900

    ⚙️ Kuma UI 入れる

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,15 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+import { withKumaUI } from "@kuma-ui/next-plugin";
 
-export default nextConfig;
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+	reactStrictMode: true,
+};
+
+const config = withKumaUI(nextConfig, {
+	// The destination to emit an actual CSS file. If not provided, the CSS will be injected via virtual modules.
+	outputDir: "./.kuma", // Optional
+	// Enable WebAssembly support for Kuma UI. Default is false and will use Babel to transpile the code.
+	wasm: true, // Optional
+});
+
+export default config;
