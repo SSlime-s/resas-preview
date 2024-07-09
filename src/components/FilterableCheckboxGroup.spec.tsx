8c7841f71fdc0132b9a384fffc1dd15d17427a35
SSlime-s <62363188+SSlime-s@users.noreply.github.com>
2024-07-10 02:17:34 +0900

    💚 FilterableCheckboxGroup にスナップショットテストいれる

---
+++ src/components/FilterableCheckboxGroup.spec.tsx
@@ -0,0 +1,32 @@
+import { render } from "@testing-library/react";
+
+import { FilterableCheckboxGroup } from "./FilterableCheckboxGroup";
+
+function setup() {
+	const options = [
+		{ label: "Foo", value: "foo" },
+		{ label: "Bar", value: "bar" },
+		{ label: "Baz", value: "baz" },
+	];
+
+	const onSelectChange = vi.fn();
+	const onFilter = vi.fn(() => options);
+	const result = render(
+		<FilterableCheckboxGroup
+			options={options}
+			selected={["foo"]}
+			onSelectChange={onSelectChange}
+			onFilter={onFilter}
+		/>
+	);
+
+	return { ...result, mock: { onSelectChange, onFilter } };
+}
+
+describe("Components/FilterableCheckboxGroup", () => {
+	it("スナップショット", () => {
+		const { container } = setup();
+
+		expect(container).toMatchSnapshot();
+	});
+});
